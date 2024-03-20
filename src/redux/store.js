679673ee17e4eac91e74ad617571d8cc82074b27
trieunnh5903 <111626907+trieunnh5903@users.noreply.github.com>
2024-03-20 15:42:40 +0700

    .

--- src/redux/store.js
+++ src/redux/store.js
@@ -24,6 +24,7 @@ export const store = configureStore({
   reducer: persistedReducer,
   middleware: getDefaultMiddleware =>
     getDefaultMiddleware({
+      immutableCheck: false,
       serializableCheck: {
         ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
       },
--- src/screens/Game.jsx
+++ src/screens/Game.jsx
@@ -2,14 +2,23 @@ import {
   Dimensions,
   Image,
   Pressable,
+  StatusBar,
   StyleSheet,
   TouchableOpacity,
   View,
 } from 'react-native';
-import React, {useCallback, useEffect, useMemo, useState} from 'react';
+import React, {
+  useCallback,
+  useEffect,
+  useLayoutEffect,
+  useMemo,
+  useState,
+} from 'react';
 import {useSelector} from 'react-redux';
 import {icons} from '../constant';
 import Animated, {
+  Easing,
+  Extrapolation,
   FadeIn,
   FadeOut,
   interpolate,
@@ -18,10 +27,12 @@ import Animated, {
   useSharedValue,
   withTiming,
 } from 'react-native-reanimated';
+import {SafeAreaView} from 'react-native-safe-area-context';
 
 const {width: screen_width, height: screen_height} = Dimensions.get('window');
 
 const Game = ({navigation}) => {
+  const timeCountDown = 3;
   const foodStore = useSelector(state => state.imageInGame);
   const level = useSelector(state => state.level);
   const time = useSelector(state => state.time);
@@ -31,9 +42,12 @@ const Game = ({navigation}) => {
   const [images, setImages] = useState([...foodStore]);
   const [selectedImages, setSelectedImages] = useState([]);
   const secondsRemaining = useSharedValue(timePlay);
-  const [win, setwin] = useState(false);
   const [selectedImage1, setSelectedImage1] = useState(false);
   const timeoutImage1Visible = React.useRef(null);
+  const [showCountDown, setShowCountDown] = useState(false);
+  const numberOfCell = level === 1 ? 16 : level === 2 ? 20 : 24;
+  const [countDown, setCountDown] = useState(timeCountDown);
+  const countDownValue = useSharedValue(0);
   const luckyGiftUri = useMemo(() => {
     return images.map(i => {
       if (i.selected === true) {
@@ -49,7 +63,55 @@ const Game = ({navigation}) => {
     );
   }, []);
 
-  const numberOfCell = level === 1 ? 16 : level === 2 ? 20 : 24;
+  useEffect(() => {
+    const BAT_DAU = 'BẮT ĐẦU';
+    const startTimer = () => {
+      secondsRemaining.value = withTiming(
+        0,
+        {duration: timePlay * 1000},
+        isFinished => {
+          if (isFinished) {
+            runOnJS(navigation.navigate)('lose');
+          }
+          runOnJS(stopTimer)();
+        },
+      );
+    };
+
+    const onCountDownAnimationFinished = () => {
+      if (countDown > 1) {
+        setCountDown(countDown - 1);
+      } else if (countDown === 1) {
+        setCountDown(BAT_DAU);
+      }
+      if (countDown !== BAT_DAU) {
+        countDownValue.value = 0;
+      } else if (countDown === BAT_DAU) {
+        setShowCountDown(false);
+        startTimer();
+      }
+    };
+
+    const startCountDown = () => {
+      if (countDown >= 1 || countDown === BAT_DAU) {
+        countDownValue.value = withTiming(
+          1,
+          {duration: 600, easing: Easing.out(Easing.quad)},
+          isFinished => {
+            if (isFinished) {
+              runOnJS(onCountDownAnimationFinished)();
+            }
+          },
+        );
+      }
+    };
+
+    if (showCountDown) {
+      startCountDown();
+    }
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [countDown, countDownValue, showCountDown]);
 
   useEffect(() => {
     if (selectedImage1) {
@@ -63,8 +125,7 @@ const Game = ({navigation}) => {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [selectedImage1]);
 
-  // console.log(images);
-  useEffect(() => {
+  useLayoutEffect(() => {
     const createNewImageArray = () => {
       const listSortImageLucky = [];
       const listSortImageUnlucky = [];
@@ -124,9 +185,10 @@ const Game = ({navigation}) => {
   }, [selectedImages]);
 
   const onSelectedSuccess = uri => {
-    stopTimer();
-    setwin(true);
     navigation.navigate('win', {uri});
+    setCountDown(timeCountDown);
+    stopTimer();
+    // setwin(true);
   };
 
   const handleImagePress = useCallback(
@@ -152,20 +214,6 @@ const Game = ({navigation}) => {
     setImages(newImages);
   };
 
-  const startTimer = useCallback(() => {
-    secondsRemaining.value = withTiming(
-      0,
-      {duration: timePlay * 1000},
-      isFinished => {
-        if (isFinished && !win) {
-          runOnJS(navigation.navigate)('lose');
-        }
-        runOnJS(stopTimer)();
-      },
-    );
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, [secondsRemaining, stopTimer, timePlay, win]);
-
   const stopTimer = useCallback(() => {
     secondsRemaining.value = timePlay;
     setIsPlaying(false);
@@ -184,9 +232,9 @@ const Game = ({navigation}) => {
     if (images) {
       setIsPlaying(true);
       setSelectedImages([]);
+      setShowCountDown(true);
       shuffleImages();
-      startTimer();
-      setwin(false);
+      // setwin(false);
     }
   };
 
@@ -199,9 +247,28 @@ const Game = ({navigation}) => {
     return {width: width};
   });
 
+  const countDownViewAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      opacity: countDownValue.value,
+      transform: [{scaleY: countDownValue.value}],
+    };
+  });
+
+  const countDownTextAnimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [{scale: countDownValue.value}],
+      fontSize: interpolate(
+        countDownValue.value,
+        [0, 1],
+        [0, 60],
+        Extrapolation.CLAMP,
+      ),
+    };
+  });
+
   return (
-    <View style={styles.container}>
-      <View style={{padding: 16}}>
+    <SafeAreaView style={styles.container}>
+      <View style={[styles.p_16]}>
         <View style={styles.timerWrapper}>
           <Animated.View style={[styles.timer, timeRemainingStyle]} />
         </View>
@@ -236,26 +303,61 @@ const Game = ({navigation}) => {
         </View>
       </View>
 
-      <TouchableOpacity
-        disabled={isPlaying}
-        style={styles.buttonStart}
-        onPress={onStartPress}>
-        <Image
-          style={{
-            height: screen_width * 0.2,
-            width: screen_width * 0.4,
-          }}
-          resizeMode="contain"
-          source={icons.btn_letgo}
-        />
-      </TouchableOpacity>
-    </View>
+      <View>
+        <TouchableOpacity
+          disabled={isPlaying}
+          style={styles.buttonStart}
+          onPress={onStartPress}>
+          <Image
+            style={{
+              height: screen_width * 0.2,
+              width: screen_width * 0.4,
+            }}
+            resizeMode="contain"
+            source={icons.btn_letgo}
+          />
+        </TouchableOpacity>
+      </View>
+
+      {showCountDown && (
+        <View style={styles.countDownWrapper}>
+          <Animated.View
+            style={[styles.countDownContainer, countDownViewAnimatedStyle]}>
+            <Animated.Text
+              style={[styles.countDownText, countDownTextAnimatedStyle]}>
+              {countDown}
+            </Animated.Text>
+          </Animated.View>
+        </View>
+      )}
+    </SafeAreaView>
   );
 };
 
 export default Game;
 
 const styles = StyleSheet.create({
+  countDownText: {
+    margin: 20,
+    fontSize: 60,
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  countDownContainer: {
+    width: '100%',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'rgba(0,0,0,0.5)',
+  },
+  countDownWrapper: {
+    ...StyleSheet.absoluteFillObject,
+    backgroundColor: 'rgba(0,0,0,0.4)',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  p_16: {
+    padding: 16,
+  },
   buttonStart: {alignSelf: 'center'},
   imageGift: {width: '100%', height: '100%', borderRadius: 6},
   gameContainer: {
@@ -299,7 +401,7 @@ const styles = StyleSheet.create({
     borderRadius: 6,
   },
   gameWrapper: {
-    height: screen_height - 30 - 64 - screen_width * 0.2,
+    height: screen_height - 62 - screen_width * 0.2 - StatusBar.currentHeight,
     overflow: 'hidden',
     gap: 20,
     paddingHorizontal: 16,
@@ -308,7 +410,6 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     alignItems: 'center',
     // flex: 1,
-    // backgroundColor: 'red',
   },
   container: {
     flex: 1,
